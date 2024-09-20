dbec8157146d19a5f1d08da38af52133d0a219fd
Morgan Jacob <jacobmmori@gmail.com>
2024-09-20 16:17:01 -0400

    Create displayEmployeeShifts function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -1,10 +1,19 @@
 // Task 1- Create an Employees Array of Employee Objects
 
 const employees = [  
-      
+
     { name: 'John', shifts: [{ day: 'Monday', hours: 8 }, { day: 'Wednesday', hours: 6 }] },
     { name: 'Sara', shifts: [{ day: 'Tuesday', hours: 5 }, { day: 'Thursday', hours: 7 }] },
     { name: 'David', shifts: [{ day: 'Monday', hours: 8 }] },
     { name: 'Emily', shifts: [{ day: 'Friday', hours: 8 }] }
 
 ];
+
+// Task 2- Create a Function to Display Employee Shift Details
+
+function displayEmployeeShifts (employees) {
+    console.log(`Employee: ${employees.name}`)
+    }
+
+
+// Task 3- 
