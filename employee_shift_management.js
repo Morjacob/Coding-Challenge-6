89036849b60c0e1b19b7aefdbf60eed7a593682c
Morgan Jacob <jacobmmori@gmail.com>
2024-09-20 16:36:05 -0400

    Create calculateTotalHours function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -15,5 +15,26 @@ function displayEmployeeShifts (employees) {
     console.log(`Employee: ${employees.name}`)
     }
 
+    employees.name[1]
 
-// Task 3- 
+// Task 3- Create a Function to Assign a New Shift
+
+function assignShift (employeesName, days, hours) {
+   const employees = employees.find(employee => employee.name === employeesName); 
+   const currentShift = employeeShift.find (shift => shifts.day === day)
+}
+
+
+// Task 4- Create a Function to Calculate Total Hours Worked
+
+function calculateTotalHours (employeesName) {
+    const employee = employees.find (employee => employee.name === employeesName);
+    for (const shift of employee.shifts) {
+        const hours = totalHours (shift.hours)
+        overallHours += hours
+    }
+    return (`${totalHours}`)
+}   
+
+console.log(calculateTotalHours[1])
+
